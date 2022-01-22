9555a23b552e100823d1bce0dd3d523614d44f38
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-22 11:17:19 -0800

    python comments

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") # requiring the csv file into this page
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): # starting a function, passing in the csv file
+    for line in log_file:   # for in loop, looping over every line in the file
+        line = line.rstrip()   # taking each line and removing all the trailing white space
+        day = line[0:3]    # setting a variable called "day" to the first 3 characters of each line
+        if day == "Mon":    # if statement asking if the "day" variable = "tue"
+            print(line)     # printing the whole line to the console, as long as it meets the above if statement conditions
 
 
 sales_reports(log_file)
